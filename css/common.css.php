f3ec201644b356e2f96568387e446b42994599cf
Madhura Jayaratne <madhura.cj@gmail.com>
2015-03-16 11:10:08 +0530

    Styles for server variables page
    Signed-off-by: Madhura Jayaratne <madhura.cj@gmail.com>

--- css/common.css.php
+++ css/common.css.php
@@ -1747,9 +1747,10 @@ div#queryAnalyzerDialog table.queryNums {
 
 /* server variables */
 #serverVariables {
-    min-width: 30em;
+    table-layout: fixed;
+    width: 100%;
 }
-#serverVariables .var-row > div {
+#serverVariables .var-row > td {
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
@@ -1760,15 +1761,17 @@ div#queryAnalyzerDialog table.queryNums {
     background: #f3f3f3;
     font-weight: bold;
 }
-#serverVariables .var-header .var-value {
+#serverVariables .var-header {
     text-align: <?php echo $left; ?>;
 }
 #serverVariables .var-row {
     padding: 0.5em;
     min-height: 18px;
 }
+#serverVariables .var-action {
+    width: 120px;
+}
 #serverVariables .var-name {
-    width: 45%;
     float: <?php echo $left; ?>;
 }
 #serverVariables .var-name.session {
